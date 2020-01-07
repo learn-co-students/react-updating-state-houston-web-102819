6940ee7a2f262f9fe25147f32152b9c2e23f1d2f
alexvancap <alexvancap@gmail.com>
2020-01-07 14:51:26 -0600

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,24 @@
-// Code ClickityClick Component Here
+
+import React from "react";
+
+export default class ClickityClick extends React.Component{
+
+    updateState = (state) => {
+        this.setState({
+            hasBeenClicked: state
+        })
+    }
+    state = {
+        hasBeenClicked: false
+    }
+    render(){
+        
+
+        return (
+            <div>
+                <p>I have {this.state.hasBeenClicked ? null : "not"} been Clicked!</p>
+                <button onClick={() => this.updateState(true)}>Click me!</button>
+            </div>
+        )
+    }
+}
